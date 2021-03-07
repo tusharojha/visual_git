7fc8cab62ce554e83a0a69c920e7cb6446841e94
Tushar Ojha <tusharojha2001@gmail.com>
2021-03-07 12:14:07 +0530

    updated header and added README file

---
+++ README.md
@@ -0,0 +1,28 @@
+## Visual Git
+
+Learn Git with proper Visualization. Usually beginners struggle to understand git commands because it's all CLI. This learning tool will help you create a strong understanding of git commands through visualizations shown of every command entered.
+
+## Technical Specifications
+
+The project is a single page web application developed in `React` and uses `gitgraphjs` as it's main dependency.
+
+## Installation
+
+- Make sure to have react is installed
+- Clone the repository
+- Open the project directory
+- Run the following commands on `git bash` or `command prompt`:
+
+```
+    npm install
+    npm start
+```
+
+This will start the `localhost:3000` server.
+
+## Contributing
+
+- Contributions are most welcome and appreciated.
+- Make sure to format all the files properly.
+- If you face any bug or problem, file an issue to this repository with proper description of the problem.
+- Add proper description about your PR's objective.
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import HamburgerMenu from "react-hamburger-menu";
 import Sidebar from "react-sidebar";
+import logo from "../../assets/logo.png";
 
 import "./header.css";
 
@@ -21,16 +22,20 @@ const Header = (props) => {
         <ul>
           <MenuItem title="Home" url="#" currentPage={props.currentPage} />
           <MenuItem title="About" url="#" currentPage={props.currentPage} />
-          <MenuItem title="Tutorial" url="#" currentPage={props.currentPage} />
+          <MenuItem
+            title="Tutorial"
+            url="https://www.youtube.com/playlist?list=PL-uHUISvCM2zlXI3FHEv-cDpoUQy4eFsY"
+            currentPage={props.currentPage}
+          />
           <li>
             <div className="logo">
-              <h2>Visual Git</h2>
+              <img src={logo} height={50} />
             </div>
           </li>
           <MenuItem title="Commands" url="#" currentPage={props.currentPage} />
           <MenuItem
             title="Source on GitHub"
-            url="#"
+            url="https://github.com/tusharojha/visual_git"
             currentPage={props.currentPage}
           />
           <MenuItem title="Share" url="#" currentPage={props.currentPage} />
@@ -38,7 +43,7 @@ const Header = (props) => {
       </div>
       <div className="mobile">
         <div className="logo">
-          <h2>Visual Git</h2>
+          <img src={logo} height={50} />
         </div>
         <Sidebar
           sidebar={
